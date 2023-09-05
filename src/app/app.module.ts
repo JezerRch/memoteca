b131bb84e87fe7fb4c18668c29f99dfee84d4124
JezerRch <jezerprocha@hotmail.com>
2023-09-05 15:42:57 -0400

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PensamentosComponent } from './componentes/pensamentos/pensamentos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PensamentosComponent
   ],
   imports: [
     BrowserModule,
